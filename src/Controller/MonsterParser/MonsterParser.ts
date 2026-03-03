5395d64185172077951d2d38b90bc14fd9ae2933
Fagner Mendes <fagnerlmendes@gmail.com>
2026-03-03 20:41:57 -0300

    Monster data stuff

--- src/Controller/MonsterParser/MonsterParser.ts
+++ src/Controller/MonsterParser/MonsterParser.ts
@@ -1,20 +1,7 @@
-import { getWeaponData } from "../../Model/Services/API/weaponGet";
+import { getMonstersData } from "../../Model/Services/API/monsterGet";
 import type { Monsters } from "../../Model/types/monsterTypes";
 
-export interface ParsedMonster extends Omit<
-  Weapons,
-  "damage" | "specials" | "crafting"
-> {
-  damage: number;
-  element: string;
-  craftable: boolean;
-}
-export const getWeapons = async (): Promise<ParsedWeapon[]> => {
-  const data = await getWeaponData();
-  return data.map((item) => ({
-    ...item,
-    damage: item.damage.display,
-    element: item.specials[0]?.element ?? "None",
-    craftable: item.crafting.craftable,
-  }));
+export const getMonsters = async (): Promise<Monsters[]> => {
+  const data = await getMonstersData();
+  return data;
 };
--- src/Model/Services/API/monsterGet.tsx
+++ src/Model/Services/API/monsterGet.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { type Weapons } from "../../types/weaponTypes";
+import { type Monsters } from "../../types/monsterTypes";
 
-export const getMonstersData = async (): Promise<Weapons[]> => {
+export const getMonstersData = async (): Promise<Monsters[]> => {
   try {
     const response = await axios({
       method: "get",
