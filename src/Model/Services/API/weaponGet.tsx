2e383efc9fd8053cd5d2f19b08105cd2e9598b60
Fagner Mendes <fagnerlmendes@gmail.com>
2026-03-03 18:29:33 -0300

    idk

---
+++ src/Model/Services/API/weaponGet.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { type Weapons } from "../../types/weaponTypes";
+
+export const getWeaponData = async (): Promise<Weapons[]> => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: "https://wilds.mhdb.io/en/weapons",
+    });
+    return response.data;
+  } catch (error) {
+    console.error("error", error);
+    throw error;
+  }
+};
